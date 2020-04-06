df29273c2904b3dee03c99d3201715b7b814f309
charcoallake <cjd3549@gmail.com>
2020-04-06 20:36:12 +0900

    2.0

--- project2/class.js
+++ project2/class.js
@@ -1,20 +1,22 @@
-let offset = 50;
 
 
 class Teapot{
 
   constructor(lv, cnt, max){
     this.lv = lv;    // level
-    this.size = ( lv != 0)? (default_size*(1/(lv+1))) : default_size;
-    this.rd = lv*80;// radius
-    this.x = 2*PI * (cnt/max);
-    this.cnt = cnt;
-    this.max = max;
-    this.rx; this.ry;
+    this.size = ( lv != 0)? (default_size*(1/(lv+1))) : default_size; // size of this teapot.
+    this.rd = lv*80; // radius ; distance between teapot and the origin
+    this.x = 2*PI * (cnt/max);  // angle of this teapot in the level.
+    this.cnt = cnt; // id of this teapot in the level.
+    this.max = max; // number of teapots in the level.
+    this.rx; this.ry; // rotation value
     
   }
   
   display(){
+    // draw teapots on the screen.
+    // positions are calculated by sin, cos function.
+    // teapots get further from the origin according to amplitude value.
     push();
     translate(sin(this.x)*(this.rd+amp.getLevel()*100*this.lv), cos(this.x)*(this.rd+amp.getLevel()*100*this.lv), 0);
     scale(this.size);
@@ -24,7 +26,7 @@ class Teapot{
     pop();
   }
   
-  rotate(){
+  rotate(){ // make teapots to rotate according to mouse x, y positions.
     this.rx = map(mouseY, 0, windowWidth, radians(-50), radians(50));
     this.ry = map(mouseX, 0, windowHeight, radians(-50), radians(50));
     rotateX(this.rx);
@@ -32,7 +34,10 @@ class Teapot{
     rotateZ(mouseY*0.01+(this.cnt/this.max));
   }
   
-  circle(){
+  circle(){ // make teapots go circle.
+    // if level is odd number, teapots go counterclockwise,
+    // if even number, they go clockwise by default.
+    // circling direction changes when dir value is changed.
     if(this.lv%2 == 1)
       this.x += dir*radians(0.1);
     else 
--- project2/sketch.js
+++ project2/sketch.js
@@ -1,33 +1,45 @@
-let px, py;
+let px, py;  // position x, position y ; mapped value of mouseX, mouseY
 
+/*
+teapots are divides into levels,
+if the level is low, the teapot(s) are closer to the center and larger.
+there is 4 levels, tp0 is teapot at the origin.
+there are 6 2-level teapots, 15 3-level teapots, 35 4-level teapots.
+*/
 let tp0;
 let tp1 = []; let tp1_cnt = 6;
 let tp2 = []; let tp2_cnt = 15;
 let tp3 = []; let tp3_cnt = 35;
 
-let dir=1;
-let default_size;
+let dir=1;   // dancing direction
+let default_size; // tp0 size ; deteremined according to windowWidth
 
-let amp;
-let ac=0, hc=0, sc=0, bc=0;
-let colorSet = [];
-let set =0;
+let amp;    // amplitude element
+let colorSet = [];  // colorSet for changing background and light colors. (double list)
+let set =0;   // index value for selecting a colorSet.
 
 
 function preload(){
+  // load assets
   teapot = loadModel("teapot.obj");
   sound = loadSound("music.mp3");
 }
 
 function setup() {
+
+  // 1. create canvas
   createCanvas(windowWidth, windowHeight, WEBGL);
+
+  // 2. set default size ; teapot size is set according to window width
   default_size = windowWidth/300;
   colorMode(RGB);
   
-  amp = new p5.Amplitude();
-  sound.play();
+  amp = new p5.Amplitude(); // p5.sound library
+  sound.play();             // start music
   
   let i;
+
+  // 3. create teapots
   tp0 = new Teapot(0, 0, 1);
   for(i=0; i<tp1_cnt; i++){
     tp1.push(new Teapot(1, i, tp1_cnt));
@@ -39,7 +51,7 @@ function setup() {
     tp3.push(new Teapot(3,i, tp3_cnt));
   }
 
-  // [background, ambient, dir1, dir2, dir3]
+  // 4. create color sets ; [background, ambientLight, directionalLight0, dir1, dir2]
   colorSet.push([color(33, 134, 140), color(20), color(138, 20, 25), color(255, 0, 0), color(190, 125, 125)]);
   colorSet.push([color(0, 0, 0), color(0, 0, 80), color(255, 0, 0), color(0, 0, 255), color(100, 0, 100)]);
   colorSet.push([color(180), color(0), color(0), color(0), color(0)]);
@@ -48,8 +60,8 @@ function setup() {
 
 function draw() {
   
-  let time;
-  
+  let time; // how much sound has been played. (0~1.0)
+
   background(colorSet[set][0]);
   noStroke();
   ambientLight(colorSet[set][1]);
@@ -57,6 +69,7 @@ function draw() {
   px = map(mouseX, 0, width, -width/2, width/2);
   py = map(mouseY, 0, height, -height/2, height/2);
 
+  // Change light directions according to mouse positions
   directionalLight(colorSet[set][2], px, py, -100);
   directionalLight(colorSet[set][3], -px, -py, -100);
   directionalLight(colorSet[set][4], -px, py, -100);
@@ -66,6 +79,8 @@ function draw() {
   specularColor(0, 0, 0);
   
   camera(mouseX*0.3, mouseY*0.3, 500, 0, 0, 0, 0, 1, 0); // camera movement
+
+  // display all the teapots
   tp0.display();
   for(i=0; i<tp1_cnt; i++)
       tp1[i].display();
@@ -84,12 +99,14 @@ function draw() {
 }
 
 function mousePressed(){
+  // change dancing direction
   dir = -dir;
 }
 
 function keyPressed(){
+  // if spacebar pressed, pause or restart music.
   if(keyCode == 32){
-    if(sound.isPlaying()) sound.pause();
+    if(sound.isPlaying()) sound.stop();
     else sound.play();
   }
 } 
